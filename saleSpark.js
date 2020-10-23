c7031533102d2eef4c6b99923e875b6d555ab04b
Aleem Ahmad <aleemahmad1987@gmail.com>
2020-10-23 19:30:51 +0500

    New Version Change

--- saleSpark.js
+++ saleSpark.js
@@ -44,7 +44,7 @@ function removeGDPR() {
 function globalJavascript() {
 
     const webApi = "https://" + window.location.hostname + "/apps/storefront/api/storefront/";
-    const scriptBase = "https://cdn.jsdelivr.net/gh/salespark/salesparkv2@1.1.1/";
+    const scriptBase = "https://cdn.jsdelivr.net/gh/salespark/salesparkv2@1.1.2/";
     var sweetAlertIncluded = false;
     var store = {};
     this.init = function (callback, callbackArgs) {
